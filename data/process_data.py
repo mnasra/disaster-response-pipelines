3216a329bea29e8fadb50e71b7a6342867bd7f25
= <22745261+mnasra@users.noreply.github.com>
2020-02-21 15:16:51 +0000

    Finished ETL pipeline and moved notebook code to process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,50 @@
+import pandas as pd
+import sqlalchemy
 import sys
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
 
+    return messages, categories
 
-def clean_data(df):
-    pass
+def clean_data(messages, categories):
+    # split the single column into 36 different columns
+    categories_split = categories['categories'].str.split(';',expand=True)
 
+    # extract the different column names by splitting at the - and taking the first value
+    category_colnames = []
+    for value in categories_split.head(1).values[0]:
+        column_name, number = value.split('-')
+        category_colnames.append(column_name)
+
+    # concatenate the original ids to the new split up dataframe and rename the columns accordingly
+    categories_expanded = pd.concat([categories['id'], categories_split], axis=1)
+    categories_expanded.columns = ['id', *category_colnames]
+
+    # Apply function above to all values in dataframe and convert to numeric
+    categories_expanded = categories_expanded.applymap(fix_categories_data)
+    categories_expanded = categories_expanded.apply(lambda x: pd.to_numeric(x),axis=1)
+
+    # Merge datasets
+    df_merged = messages.merge(categories_expanded, how='inner', on='id')
+
+    #Remove duplicates
+    # We will arbitrarily pick the first value as there are only 68 cases out of 26248
+    df_clean = df_merged.loc[~df_merged['id'].duplicated(keep='first')]
+    return df_clean
+
+#function to apply to all cells to strip the unnecessary words
+def fix_categories_data(cell):
+    if '-' in str(cell): #Condition put in to avoid errors with the id column
+        return cell.split('-')[1]
+    else:
+        return cell
 
 def save_data(df, database_filename):
-    pass  
-
+    engine = sqlalchemy.create_engine('sqlite:///'+database_filename)
+    df.to_sql('DisasterResponseTable', engine, index=False)
 
 def main():
     if len(sys.argv) == 4:
@@ -20,10 +53,10 @@ def main():
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
+        messages, categories = load_data(messages_filepath, categories_filepath)
 
         print('Cleaning data...')
-        df = clean_data(df)
+        df = clean_data(messages, categories)
         
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
