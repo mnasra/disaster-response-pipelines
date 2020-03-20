f6964dfc5f5678f951818c17d23d8511f17df5a1
= <22745261+mnasra@users.noreply.github.com>
2020-03-20 21:35:32 +0000

    Cleaned up repo of useless files

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,119 @@
 import sys
-
+import pandas as pd
+import sqlalchemy
+import re
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+import pickle
 
 def load_data(database_filepath):
-    pass
+    """Loads data from SQLite database and splits to X and Y dataframes
 
+    Keyword arguments:
+    database_filepath - string containing file path for SQLite databse
+    
+    Returns:
+    X - pandas dataframe containing the message posted by users
+    Y - pandas dataframe containing binary values that indicate whether the text
+        is part of that category
+    category_names - array of strings that contain the names of the categories
+    """
+    engine = sqlalchemy.create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('Message', engine)
+    X = df['message']
+    Y = df.iloc[:,5:] #Skip genre and add later after one hot encoding
+    Y_dummies = pd.get_dummies(df[['genre']].astype(str))
+    Y = Y.merge(Y_dummies, how='inner',left_index=True,right_index=True, validate='1:1')
+    Y['related'] = Y['related'].apply(lambda x: 1 if x==2 else x) #Change 2s into 1s so this is binary
+    category_names = Y.columns
+    return X, Y, category_names
 
 def tokenize(text):
-    pass
+    """Takes a piece of text and does the following:
+        - Removes stop words
+        - Lemmatizes words
+        - Removes punctuation
+        - Lower cases all words
+        - Tokenizes words
+
+    Keyword arguments:
+    text - string containing piece of text
+    
+    Returns:
+    tokens - array containing tokens associated with that text
+    """
+    stop_words = stopwords.words("english")
+    lemmatizer = WordNetLemmatizer()
+    
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # lemmatize andremove stop words
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+
+    return tokens
 
 
 def build_model():
-    pass
+    """Builds a machine learning pipeline and sets up grid search ready to be trained
 
+    Returns:
+    model - GridSearchCV object ready for training on training data
+    """
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(KNeighborsClassifier()))
+    ])
+    
+    parameters = {
+        'vect__ngram_range':[(1,1),(1,2)],
+        'tfidf__use_idf': (True, False), 
+        'clf__estimator__p':[1,2]
+    }
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    model = GridSearchCV(pipeline, parameters)
+    return model
 
+def evaluate_model(model, X_test, y_test, category_names):
+    """ Evaluates the model by printing f1 score, precision, and recall
+
+    Keyword arguments:
+    model - model object that was used to make predictions
+    X_test - Pandas dataframe containing testing data from the X dataframe
+    y_test - Pandas dataframe containing testing data from the Y dataframe
+    category_names - category_names - array of strings that contain the names of the categories 
+    """
+    y_pred = model.predict(X_test)
+    for i in range(y_pred.shape[1]):
+        col_name = y_test.columns[i]
+        if col_name in category_names:
+            y_test_i = y_test.iloc[:,i]
+            y_pred_i = [item[i] for item in y_pred]
+            print(col_name + ":")
+            print(classification_report(y_test_i, y_pred_i))
 
 def save_model(model, model_filepath):
-    pass
+    """ Saves the model in the specified file path using pickle
+
+    Keyword arguments:
+    model - model object that was used to make predictions
+    model_filepath - String containing location you wish to save model
+    """
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 
 def main():
